62dffbfbef8db8ab6c208e80ebb8f0e77f936d0b
arman <am43malik@gmail.com>
2022-07-15 20:08:32 +0530

    make change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './images/logo.png'
+import notification from './images/notification.png'
+import inbox from './images/inbox.png'
+import video from './images/video.png'
+import search from './images/search.png'
+import profile from './images/profile-pic.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <body>
+    <section>
+        <nav>
+            <div class="nav-left">
+                <img src={logo} alt="" class="logo"/>
+
+                <ul>
+                    <li><img src={notification} alt=""/>
+
+                        
+                    </li>
+                    <li> <img src={inbox} alt=""/></li>
+
+                           <li> <img src={video} alt=""/></li>
+                    <li></li>
+
+                </ul>
+            </div>
+            <div class="nav-right">
+                <div class="search-box">
+                    <img src={search} alt=""/>
+                    <input type="text" placeholder="search"/>   
+                
+                </div>
+                <div class="nav-user-icon">
+                    <img src={profile} alt=""/>
+                </div>
+            </div>
+        </nav>
+       
+        
+    </section>
+</body>
+   </div>
   );
 }
 
